bdccec7b2889f67eba2ded36663b91c8775579f1
jicsontoh <jicsontoh@gmail.com>
2023-07-21 17:37:40 +0800

    Update app setHeader

--- app.js
+++ app.js
@@ -12,13 +12,23 @@ dotenv.config();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 app.use("/api/places", placesRoutes);
 
 app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
   const error = new HttpError("Could not find such path", 404);
-  throw error;
+  next(error);
 });
 
 app.use((error, req, res, next) => {
