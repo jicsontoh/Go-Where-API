e9ecffb4687fe0b22c934c12968b259785c948ae
jicsontoh <jicsontoh@gmail.com>
2023-07-23 14:17:17 +0800

    Add hash password

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,6 +1,7 @@
 const uuid = require("uuid");
 const HttpError = require("../models/http-error");
 const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 
 const getUsers = async (req, res, next) => {
@@ -40,11 +41,19 @@ const signup = async (req, res, next) => {
     return next(error);
   }
 
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+    const error = new HttpError("Could not create user", 500);
+    return next(error);
+  }
+
   const createdUser = new User({
     name,
     email,
     image: req.file.path,
-    password,
+    password: hashedPassword,
     places: [],
   });
 
@@ -70,7 +79,20 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
-  if (!existingUser || existingUser.password !== password) {
+  if (!existingUser) {
+    const error = new HttpError("Incorrect email or password", 401);
+    return next(error);
+  }
+
+  let isValidPassword = false;
+  try {
+    isValidPassword = await bcrypt.compare(password, existingUser.password);
+  } catch (err) {
+    const error = new HttpError("Logging in failed", 500);
+    return next(error);
+  }
+
+  if (!isValidPassword) {
     const error = new HttpError("Incorrect email or password", 401);
     return next(error);
   }
--- uploads/images/README.md
+++ uploads/images/README.md
@@ -1,4 +1,5 @@
 # Important!
 
 DO NOT delete this directory!
+
 This directory is used to store all user/place images locally which will be referenced by the backend
